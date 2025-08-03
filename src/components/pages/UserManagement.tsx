d295e0dda9ed91be3b455ab436e41098ed90cbf4
hello-yuki0409 <bobu2784@gmail.com>
2025-08-03 14:09:49 +0900

    ユーザー一覧取得機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,33 @@
-import { memo, type FC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, type FC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
-  return <p>ユーザー管理ページ</p>;
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://picsum.photos/160"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,25 @@
+import { useCallback, useState } from "react";
+import type { User } from "../types/api/user";
+import axios from "axios";
+import { useMessage } from "./useMessage";
+
+export const useAllUsers = () => {
+  const { showMessage } = useMessage();
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [users, setUsers] = useState<Array<User>>([]);
+
+  // 他のコンポーネントで使う際に再生成されないようにuseCallback
+  const getUsers = useCallback(() => {
+    setLoading(true);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch(() =>
+        showMessage({ title: "ユーザー取得に失敗しました", status: "error" })
+      )
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { getUsers, loading, users };
+};
